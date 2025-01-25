eba9a2a586e380cb0ee18a732b562521c383d728
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-25 12:48:02 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,13 @@
+public class Pattern1 {
+    public static void main(String[] args){
+    
+        int i,j;
+        for(i=0;i<=4;i++){
+            for(j=0;j<=i;j++){
+                System.out.print("x");
+            }
+            System.out.println("");
+
+    }
+}
+}

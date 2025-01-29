6d254573b05e23ba9cd71a50e227106b5e7c446e
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:26:25 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -1,13 +1,17 @@
 public class Pattern1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
     
+        // Declare variables i and j 
         int i,j;
-        for(i=0;i<=4;i++){
-            for(j=0;j<=i;j++){
+        // Outer loop controls the number of rows (5 rows in total)
+        for(i = 0; i <= 4; i++) {
+            // Inner loop controls the number of 'x' printed in each row
+            for(j = 0; j <= i; j++) {
+                // Print 'x' without moving to the next line
                 System.out.print("x");
             }
+            // Move to the next line after printing all 'x' in a row
             System.out.println("");
-
+        }
     }
-}
 }
